65272824d3de3fbd011d8455d7190c044184bd99
Bruno Vigel <brunorosav@gmail.com>
2021-12-27 08:42:45 -0300

    finalizacao addClass

--- animais-e-florestas_inicio/js/app.js
+++ animais-e-florestas_inicio/js/app.js
@@ -1,25 +1,24 @@
-const classActive = 'active'
+$('[data-group]').each(function() {
+    const $allTarget = $(this).find('[data-target]'),
+          $allClick = $(this).find('[data-click]'),
+          activeClass = 'active'
 
-$('.animais .tab-menu a').first().addClass(classActive)
-$('.animais .item').first().addClass(classActive)
+    $allTarget.first().addClass(activeClass);
+    $allClick.first().addClass(activeClass);
 
-$('.animais .tab-menu a').on('click', function(e) {
-    e.preventDefault();
-    const itemId = $(this).attr('href')
 
-    $('.animais .tab-menu a, .animais .item').removeClass(classActive)
-    $(this).addClass(classActive);
-    $(itemId).addClass(classActive);
-})
+    $allClick.on('click', function(e) {
+        e.preventDefault();
 
-$('.florestas .tab-menu a').first().addClass(classActive)
-$('.florestas .item').first().addClass(classActive)
+        const id = $(this).data('click'),
+              $target = $('[data-target="' + id + '"]')
 
-$('.florestas .tab-menu a').on('click', function(e) {
-    e.preventDefault();
-    const itemId = $(this).attr('href')
 
-    $('.florestas .tab-menu a, .florestas .item').removeClass(classActive)
-    $(this).addClass(classActive);
-    $(itemId).addClass(classActive);
+        $allClick.removeClass(activeClass);
+        $allTarget.removeClass(activeClass);
+
+        $target.addClass(activeClass);
+        $(this).addClass(activeClass)
+
+    })
 })
